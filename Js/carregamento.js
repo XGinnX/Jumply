338a00594702c29706a2903d0fb29e0a9d6c4e1c
Jean <jean.sls.snts@gmail.com>
2025-07-16 16:32:46 -0300

    feat: carrocel

--- Js/carregamento.js
+++ Js/carregamento.js
@@ -1,21 +1,21 @@
-    function loadPage(page) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', page, true);
+function loadPage(page) {
+    var xhr = new XMLHttpRequest();
+    const url = './Pages/' + page;
+    xhr.open('GET', url, true);
 
-        xhr.onload = function() {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                document.getElementById('content').innerHTML = xhr.responseText;
-            } else {
-                document.getElementById('content').innerHTML = "<p>Erro ao carregar a página.</p>";
-            }
-        };
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            document.getElementById('content').innerHTML = xhr.responseText;
+        } else {
+            document.getElementById('content').innerHTML = "<p>Erro ao carregar a página.</p>";
+        }
+    };
 
-        xhr.onerror = function() {
-            document.getElementById('content').innerHTML = "<p>Erro de conexão.</p>";
-        };
+    xhr.onerror = function () {
+        document.getElementById('content').innerHTML = "<p>Erro de conexão.</p>";
+    };
 
-        xhr.send();
-    }
+    xhr.send();
+}
 
 
-   
